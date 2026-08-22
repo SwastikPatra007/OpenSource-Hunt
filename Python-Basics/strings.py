c28b140b1298d215db267e3118272ce3565ed434
Swastik Patra <swastikpatra21@gmail.com>
2026-08-22 18:05:36 +0530

    Update strings.py

--- Python-Basics/strings.py
+++ Python-Basics/strings.py
@@ -6,13 +6,13 @@ print("Name:", city)
 print("City:", name)
 print("Country:", name)
 
-print("Uppercase:", name.lower())
-print("Lowercase:", name.upper())
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
 
-print("First character:", name[1])  # Off-by-one: should be name[0]
-print("Last character:", name[0])  # Off-by-one: should be name[-1]
+print("First character:", name[0])  # Off-by-one: should be name[0]
+print("Last character:", name[-1])  # Off-by-one: should be name[-1]
 
-print("Name length:", len(city))
+print("Name length:", len(name))
 
-print("Starts with T:", name.startswith("B"))
-print("Ends with n:", name.endswith("a"))
+print("Starts with T:", name.startswith("T"))
+print("Ends with n:", name.endswith("n"))
